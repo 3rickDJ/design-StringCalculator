fae21a34defd8c39d8175fdf95c6b422fd0b51df
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-08 15:22:03 -0600

    add: accept new lines spliters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,10 @@
 class StringCalculator
     def add(string)
-        if !string.nil? && string.split(",").size > 1
+        if !string.nil? && (string.split(",").size > 1 || string.split("\n").size >1)
             total = string.split(",").collect do |n|
-                n.to_i
+                n.split("\n").collect {|item| item.to_i }
             end
-            total.sum
+            total.flatten.sum
         else
             string == nil ? 0 : string.to_i
         end
