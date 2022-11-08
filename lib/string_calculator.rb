9ffcb4653f9cf4836ffc000468bbd1f235b40e1f
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-08 14:47:26 -0600

    refactor: String Calculatro add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,10 @@
 class StringCalculator
     def add(string)
-        string == nil ? 0 : string.to_i
+        if !string.nil? && string.split(",").size > 1
+            num1, num2 = string.split(",")
+            num1.to_i + num2.to_i
+        else
+            string == nil ? 0 : string.to_i
+        end
     end
 end
