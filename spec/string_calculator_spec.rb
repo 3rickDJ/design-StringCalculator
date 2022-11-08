50a0ab463e1fa574b68524a490302043712d28f4
3rick <erick.dejesus1@alumno.buap.mx>
2022-11-08 14:21:01 -0600

    test: new test for string_calculator

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,19 @@
+require "../lib/string_calculator"
+describe StringCalculator do
+    it "returns zero" do
+        calculator = StringCalculator.new
+        result = calculator.add("")
+        expect(result).to eq(0)
+        result = calculator.add(nil)
+        expect(result).to eq(0)
+    end
+    it "returns the same number" do
+        calculator = StringCalculator.new
+        result = calculator.add("2")
+        expect(result).to eq(2)
+        result = calculator.add("8")
+        expect(result).to eq(8)
+        result = calculator.add("99")
+        expect(result).to eq(99)
+    end
+end
