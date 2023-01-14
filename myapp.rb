eb8cb9655becf6d1ac243cc4c1b75a3f7d137c5b
Hector Garcia Rosales <ghectormus@gmail.com>
2023-01-14 13:43:02 -0600

    add: applying to the get method of sinatra the logic

--- myapp.rb
+++ myapp.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
+require_relative 'lib/string_calculator.rb'
 
 get '/' do  
+  resultado = params['input']
+  @calculator = StringCalculator.new.add(resultado)
   erb :index
 end
 
-post '/' do  
-
-  "Hello #{params['input']}!"
+post '/' do    
   redirect to("/")
 end
